987ad033c0dd962352492cde22630b72164b88ac
Michael Fulbright <mike.fulbright@pobox.com>
2018-09-25 23:03:13 -0400

    Add conda-built scripts

---
+++ bld.bat
@@ -0,0 +1,17 @@
+echo "BUILD SCRIPT RUNNING"
+
+:: add to path to find whatever bash we're going to use!
+:: SET PATH=%PATH%;C:\Users\msf\AppData\Local\Programs\Git\bin
+:: bash -c "echo 'BASH RUN TEST'"
+
+echo %PREFIX%
+
+::mkdir %PREFIX%\Scripts
+mkdir %PREFIX%\Lib\site-packages\pyastroimageview
+
+:: copy without extension so conda-build will make .bat file for it!
+copy Scripts\pyastroimageview_main.py %PREFIX%\Scripts\pyastroimageview_main
+
+dir %PREFIX%\Scripts\
+
+xcopy /s pyastroimageview %PREFIX%\Lib\site-packages\pyastroimageview
