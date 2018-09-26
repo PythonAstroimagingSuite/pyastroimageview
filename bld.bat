39228929805c8fe78f69478b7f72c4f91236a198
Michael Fulbright <mike.fulbright@pobox.com>
2018-09-26 11:52:01 -0400

    inject build string into source

--- bld.bat
+++ bld.bat
@@ -4,6 +4,11 @@ echo "BUILD SCRIPT RUNNING"
 :: SET PATH=%PATH%;C:\Users\msf\AppData\Local\Programs\Git\bin
 :: bash -c "echo 'BASH RUN TEST'"
 
+echo %PKG_BUILDNUM%
+echo %PKG_NAME%
+echo %PKG_VERSION%
+echo %PKG_HASH%
+echo %CONDA_PY%
 
 ::mkdir %PREFIX%\Scripts
 mkdir %PREFIX%\Lib\site-packages\pyastroimageview
@@ -14,3 +19,6 @@ copy Scripts\pyastroimageview_main.py %PREFIX%\Scripts\pyastroimageview_main
 
 xcopy /s pyastroimageview %PREFIX%\Lib\site-packages\pyastroimageview
 xcopy /s pyastrobackend %PREFIX%\Lib\site-packages\pyastrobackend
+
+:: put version in sources so we can report it when program is run
+echo VERSION='py%CONDA_PY%%GIT_DESCRIBE_HASH%_%GIT_DESCRIBE_NUMBER%' >> %PREFIX%\Lib\site-packages\pyastroimageview\build_version.py
