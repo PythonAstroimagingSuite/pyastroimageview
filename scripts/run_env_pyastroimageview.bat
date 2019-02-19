ae6fbf3d6829ac94bf0e82c49524c49afe26311b
msf <mike.fulbright@pobox.com>
2019-02-19 17:08:49 -0500

    Find Anaconda3 automatically

--- scripts/run_env_pyastroimageview.bat
+++ scripts/run_env_pyastroimageview.bat
@@ -1,26 +1,36 @@
 @echo off
 
-call C:\Users\msf\Anaconda3\Scripts\activate.bat C:\Users\msf\Anaconda3
+REM Find Anaconda3
+
+REM First as user then global
+if exist %HOMEPATH%\Anaconda3 (
+  echo Found in user path
+  set ANACONDA_LOC=%HOMEPATH%\Anaconda3
+) else (
+    if exist C:\Anaconda3 (
+       echo Found in root path
+       set ANACONDA_LOC="C:\Anaconda3"
+    ) else (
+       start "" /wait cmd /c "echo Cannot find Anaconda3!&echo(&pause"
+       exit
+    )
+)
+
+call %ANACONDA_LOC%\Scripts\activate.bat C:\Anaconda3
 
 echo Running DEBUG mode from git checkout
 
 REM Expects env on command line
 
 if ["%~1"]==[""] (
-  start "" /wait cmd /c "echo Pass ENV name in shortcu!&echo(&pause"
+  start "" /wait cmd /c "echo Pass ENV name in shortcut!&echo(&pause"
   exit
 )
 echo Activating %1
 call activate %1
 
-echo PATH=%PATH%
-echo PYTHONPATH=%PYTHONPATH%
-
 set PATH=../../pystarutils/scripts/;%PATH%
 set PYTHONPATH=../../pystarutils;..;%PYTHONPATH%
-
-echo PATH=%PATH%
-echo PYTHONPATH=%PYTHONPATH%
 
 python.exe -u pyastroimageview_main.py
 
