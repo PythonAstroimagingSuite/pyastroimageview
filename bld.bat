95e898ba75cb5465d202a6e6e971dc876c3a34fd
Michael Fulbright <mike.fulbright@pobox.com>
2020-10-31 16:25:03 -0400

    Working on conda packaging

--- bld.bat
+++ bld.bat
@@ -10,17 +10,13 @@ echo "BUILD SCRIPT RUNNING"
 :: echo %PKG_HASH%
 :: echo %CONDA_PY%
 
-set
-
 ::mkdir %PREFIX%\Scripts
 mkdir %PREFIX%\Lib\site-packages\pyastroimageview
-mkdir %PREFIX%\Lib\site-packages\pyastrobackend
 
 :: copy without extension so conda-build will make .bat file for it!
 copy Scripts\pyastroimageview_main.py %PREFIX%\Scripts\pyastroimageview_main
 
 xcopy /s pyastroimageview %PREFIX%\Lib\site-packages\pyastroimageview
-xcopy /s pyastrobackend %PREFIX%\Lib\site-packages\pyastrobackend
 
 :: put version in sources so we can report it when program is run
 echo VERSION='%PKG_VERSION%-py%CONDA_PY%%GIT_DESCRIBE_HASH%_%GIT_DESCRIBE_NUMBER%' >> %PREFIX%\Lib\site-packages\pyastroimageview\build_version.py
